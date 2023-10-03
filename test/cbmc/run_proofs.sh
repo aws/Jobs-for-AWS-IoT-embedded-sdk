b288b188a2d5317706ec1d755ff06c8ba1e27e1d
bradleysmith23 <bsmith21@bu.edu>
2023-10-03 16:02:07 -0700

    Update command to clone coreJSON

--- test/cbmc/run_proofs.sh
+++ test/cbmc/run_proofs.sh
@@ -6,7 +6,7 @@ UNWIND_COUNT=${UNWIND_COUNT:-10}
 
 #If coreJSON not found, clone it
 if [ ! -d "$coreJSONDir" ]; then
-    git clone https://github.com/FreeRTOS/coreJSON.git
+    git clone https://github.com/FreeRTOS/coreJSON.git --depth 1
 fi
 
 cd "$(dirname -- "$0")/../../" || exit 1
