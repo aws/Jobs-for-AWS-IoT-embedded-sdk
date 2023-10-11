4c15cdf51c89037c3463f3fce7fff9718be18d1e
bradleysmith23 <bsmith21@bu.edu>
2023-10-11 14:50:23 -0700

    Fix build error for proofs

--- test/cbmc/proofs.c
+++ test/cbmc/proofs.c
@@ -389,7 +389,7 @@ void proof_Jobs_UpdateMsg( void )
     __CPROVER_assume( bufferLength <= 64 );
     buffer = malloc( bufferLength );
 
-    __CPROVER_assume( status >= QUEUED && status <= REJECTED );
+    __CPROVER_assume( status >= 0 && status <= 4 );
 
     ret = Jobs_UpdateMsg( status,
                           expectedVersion,
