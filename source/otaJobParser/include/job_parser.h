5f82d8dd14db4884373100dc3d71bed7a51c085d
GitHub Action <action@github.com>
2023-11-10 23:19:56 +0000

    Uncrustify: triggered by comment.

--- source/otaJobParser/include/job_parser.h
+++ source/otaJobParser/include/job_parser.h
@@ -19,19 +19,19 @@
  */
 typedef struct
 {
-    const char * signature;   /**< Code Signing Signature */
-    size_t signatureLen;      /**< Length of signature */
-    const char * filepath;    /**< File path to store OTA Update on device */
-    size_t filepathLen;       /**< Length of filepath */
-    const char * certfile;    /**< Path to Code Signing Certificate on Device */
-    size_t certfileLen;       /**< Length of certfile */
-    const char * authScheme;  /**< Authentication Scheme for HTTP URL ( null for MQTT  )*/
-    size_t authSchemeLen;     /**< Length of authScheme */
-    const char * imageRef;    /**< MQTT Stream or HTTP URL */
-    size_t imageRefLen;       /**< Length of imageRef */
-    uint32_t fileId;          /**< File ID */
-    uint32_t fileSize;        /**< Size of the OTA Update */
-    uint32_t fileType;        /**< FIle Type */
+    const char * signature;  /**< Code Signing Signature */
+    size_t signatureLen;     /**< Length of signature */
+    const char * filepath;   /**< File path to store OTA Update on device */
+    size_t filepathLen;      /**< Length of filepath */
+    const char * certfile;   /**< Path to Code Signing Certificate on Device */
+    size_t certfileLen;      /**< Length of certfile */
+    const char * authScheme; /**< Authentication Scheme for HTTP URL ( null for MQTT  )*/
+    size_t authSchemeLen;    /**< Length of authScheme */
+    const char * imageRef;   /**< MQTT Stream or HTTP URL */
+    size_t imageRefLen;      /**< Length of imageRef */
+    uint32_t fileId;         /**< File ID */
+    uint32_t fileSize;       /**< Size of the OTA Update */
+    uint32_t fileType;       /**< FIle Type */
 } AfrOtaJobDocumentFields_t;
 
 /**
