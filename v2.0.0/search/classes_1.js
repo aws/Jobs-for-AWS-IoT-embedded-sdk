c1e1aad61700993db2c5bd01a505f7d5405139a2
DakshitBabbar <DakshitBabbar@users.noreply.github.com>
2025-03-10 08:51:57 +0000

    Doxygen for 2815aca80855208df81d69881191245f484ae0b2

---
+++ v2.0.0/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['jobsupdaterequest_5ft_0',['JobsUpdateRequest_t',['../struct_jobs_update_request__t.html',1,'']]]
+];
---
+++ v2.0.0/search/variables_5.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['signature_0',['signature',['../struct_afr_ota_job_document_fields__t.html#aab8415e7ecec620637fe543db400abb5',1,'AfrOtaJobDocumentFields_t']]],
+  ['signaturelen_1',['signatureLen',['../struct_afr_ota_job_document_fields__t.html#ad2e9f26c31396442b32fad410a4075dd',1,'AfrOtaJobDocumentFields_t']]],
+  ['status_2',['status',['../struct_jobs_update_request__t.html#ad1553cb9dd5d2f5761b061d8f187e0fc',1,'JobsUpdateRequest_t']]],
+  ['statusdetails_3',['statusDetails',['../struct_jobs_update_request__t.html#ac047ebf5bd87b8ec5fe7b74c90a7ea21',1,'JobsUpdateRequest_t']]],
+  ['statusdetailslength_4',['statusDetailsLength',['../struct_jobs_update_request__t.html#a6ff4a6f43c475d27331fa582b2b9aa37',1,'JobsUpdateRequest_t']]]
+];
